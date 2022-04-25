20727e68efc0718652eb61bd5bc99d1590bd1bf2
Lola <zxa011023@gmail.com>
2022-04-25 23:46:52 +0800

    Add: react-09

---
+++ react-09/src/components/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <>
+            <h2>Home</h2>
+            <div>
+                <div className="f:48 font-weight:900 f:blue-44:first-letter f:blue-44:last-letter">Hello World !</div>
+                <button className="bg:blue-47 bg:blue-54:hover bg:blue-68:active f:14 f:medium f:white h:40 ls:1 px:24 r:3 mt:12">GO to sign in</button>
+            </div>
+        </>
+    );
+}
+export default Home;
